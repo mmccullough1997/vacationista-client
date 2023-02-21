cf274d45d1ea0cb081fe6b08dc2def769e21ac8a
Mitch McCullough <mitchmccullough@gmail.com>
2023-02-21 08:33:05 -0600

    commiting FE Yelp - need to refactor in BE

--- utils/data/recommendationData.js
+++ utils/data/recommendationData.js
@@ -1,6 +1,6 @@
 // using CORSANYWHERE proxy server - can implement with backend server-side?
 const yelpToken = 'UuuiOj7OcEYoAexi8BMLw_FR9BDsNTVOjVMTv00XhrpGkNaPeOK4rSbWZIq95WmddCcM8yGW6_zShxVr54Qx9H3Z-7Yrfe9kZH97I3LnDXwfQzfJ7dA7wnCPkvXqY3Yx';
-const yelpUrl = 'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?';
+const yelpUrl = 'https://api.yelp.com/v3/businesses/search?';
 
 const getRecommendations = (location, numberOfResults) => new Promise((resolve, reject) => {
   fetch(`${yelpUrl}location=${location}&sort_by=best_match&limit=${numberOfResults}`, {
